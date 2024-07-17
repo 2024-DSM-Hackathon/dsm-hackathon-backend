856bf250f73c077e8261d624f5d4258026d71af8
하이 <kkm07133@naver.com>
2024-07-17 09:24:40 +0900

    add passwordEncoder

--- dsm-hackathon-backend/src/main/kotlin/org/dsmhackathonbackend/global/config/SecurityConfig.kt
+++ dsm-hackathon-backend/src/main/kotlin/org/dsmhackathonbackend/global/config/SecurityConfig.kt
@@ -2,17 +2,26 @@ package org.dsmhackathonbackend.global.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @EnableWebSecurity
 @Configuration
-class SecurityConfig(
+class SecurityConfig (
     private val filterConfig: FilterConfig,
-) {
+)  {
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
 
     @Bean
     @Throws(Exception::class)
@@ -33,7 +42,7 @@ class SecurityConfig(
         }
 
         http.apply(filterConfig)
-
+        
         return http.build()
     }
 }
